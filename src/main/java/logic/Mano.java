b21d31d5ff712131f46e3d646bdae9058c807d76
ShangYuQiu <101433056+ShangYuQiu@users.noreply.github.com>
2023-09-21 16:45:18 +0200

    Add files via upload

---
+++ src/main/java/logic/Mano.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logic;
+
+
+import objects.Carta;
+import java.util.*;
+/**
+ *
+ * @author shangyu
+ */
+public class Mano {
+ 
+ 
+    private ArrayList <Carta> cartas ;
+     /*
+    Crear objetos de las clases comprobaciones 
+ 
+    
+    
+    */
+    public void aaa(){
+    //Collections.sort(cartas);
+   
+    }
+}
