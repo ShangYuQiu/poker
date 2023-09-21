3b220b8c5ae8e67a640096a44aac54c81274e427
ShangYuQiu <shangyuq@ucm.es>
2023-09-21 14:29:19 +0200

    Initial commit

---
+++ src/main/java/control/commands/apartado1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package control.commands;
+
+/**
+ *
+ * @author shangyu
+ */
+public class apartado1 {
+    
+}
---
+++ src/main/java/control/controller.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package control;
+
+/**
+ *
+ * @author shangyu
+ */
+public class controller {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
