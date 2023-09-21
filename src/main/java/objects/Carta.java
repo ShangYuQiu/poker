c5022b188f2c0fe12cbf5d2d64ba911512741f42
ShangYuQiu <101433056+ShangYuQiu@users.noreply.github.com>
2023-09-21 18:18:48 +0200

    Update Carta.java

--- src/main/java/objects/Carta.java
+++ src/main/java/objects/Carta.java
@@ -8,6 +8,39 @@ package objects;
  *
  * @author shangyu
  */
-public class Carta {
+public class Carta implements Comparable<Carta>{
     
+    private int numero;
+    private String palo;
+    private String valor;
+    
+    
+    public int getNum(){
+    return numero;
+    }
+    
+    public String palo(){
+    return palo;
+    }
+    
+    public String valor(){
+    return valor;
+    }
+
+    @Override
+    public int compareTo(Carta c) {
+        
+        if ( this.numero < c.getNum()){
+        return -1;
+        }
+        
+        else if ( this.numero == c.getNum()){
+        return 0;}
+        
+        else {
+        
+        return 1;}
+    }
+    
+
 }
