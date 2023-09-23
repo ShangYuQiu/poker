ffb3f1cc9ed3a3735460dc924b944b0c4956f40d
ShangYuQiu <101433056+ShangYuQiu@users.noreply.github.com>
2023-09-24 01:40:51 +0200

    Update apartado1.java

--- src/main/java/control/commands/apartado1.java
+++ src/main/java/control/commands/apartado1.java
@@ -4,16 +4,53 @@
  */
 package control.commands;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import logic.Mano;
+import objects.Carta;
+
 /**
  *
  * @author shangyu
  */
 public class Apartado1 {
-    private String carta;
-    public Apartado1(String carta){
-        this.carta=carta;
+    
+    private String entrada;
+    private Mano mano;
+    
+    public Apartado1(String entra , String sal){
+        this.entrada=entra;
+        
+        
     }
+    
     public void execute(){
-        System.out.println(carta);
+        
+        try{
+	        File f = new File(entrada);
+                        FileInputStream fis = new FileInputStream(f);
+			InputStreamReader fr = new InputStreamReader(fis);
+	        	BufferedReader in = new BufferedReader(fr);
+                            
+                        String carta;
+                        while ( (carta=in.readLine()) != null){
+                        
+                            Mano m = new Mano ();
+                            for (int i = 0; i < carta.length(); i+=2){
+                            // no se si esta bien
+                                 Carta c = new Carta (carta.substring(i,i+1), carta.substring(i+1,i+2));
+                                 m.anniadirCart(c);
+                            }
+                        
+                        }
+                    
+                        
+                        
+                        
+        }catch(Exception e){ 
+            e.printStackTrace();
+        }
     }
 }
