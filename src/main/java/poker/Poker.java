285dd547e98016cf550229c4a8b8f92eb58c39fa
XinXiangLinZhou <100683297+XinXiangLinZhou@users.noreply.github.com>
2023-09-23 19:43:22 +0200

    Update Poker.java

--- src/main/java/poker/Poker.java
+++ src/main/java/poker/Poker.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package poker;
-import control.Controller;
+import control.controller;
 /**
  *
  * @author shangyu
