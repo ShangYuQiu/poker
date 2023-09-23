59eb691f68cc2cd0e17d6eed5fe625ed8303e4ae
ShangYuQiu <101433056+ShangYuQiu@users.noreply.github.com>
2023-09-24 01:40:25 +0200

    Update controller.java

--- src/main/java/control/controller.java
+++ src/main/java/control/controller.java
@@ -10,54 +10,42 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import control.commands.apartado1;
+import control.commands.Apartado1;
+import logic.Mano;
+import objects.Carta;
 /**
  *
  * @author shangyu
  */
 public class Controller {
+    
     private int nivel;
     private String entrada;
     private String salida;
     private String carta;
+    
+    
     public Controller(int nivel,String entrada,String salida){
+        
         this.nivel=nivel;
         this.entrada=entrada;
         this.salida=salida;
     }
-    public void cargar1(String entrada){
-        try{
-	        File f = new File(entrada);
-                        FileInputStream fis = new FileInputStream(f);
-			InputStreamReader fr = new InputStreamReader(fis);
-	        	BufferedReader in = new BufferedReader(fr);
-	        	while ( (carta=in.readLine()) != null){
-                        
-                            Mano mano = new Mano ();
-                            for (int i = 0; i < carta.length(); i+=2){
-                            // no se si esta bien
-                                 Carta c = new Carta (carta.substring(i,i+1), carta.substring(i+1,i+2));
-                                 mano.anniadirCart(c);
-                            }
-                        
-                        }
-                        
-        }catch(Exception e){ 
-            e.printStackTrace();
-        }          
-	 
-    }
+
     public void run(){
-        if(nivel==1){
-            cargar1(entrada);
-            Apartado1 apartado1=new Apartado1(carta);
-            apartado1.execute();
-        }
-        else if(nivel==2){
+        
+        switch (nivel) {
+            case 1:
             
-        }
-        else if(nivel==3){
-            
+                Apartado1 apartado1=new Apartado1(entrada, salida);
+                apartado1.execute();
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                break;
         }
     }
 }
