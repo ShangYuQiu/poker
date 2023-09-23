188e9a8a095a80892f4796b2678bc3b086fcbb1f
ShangYuQiu <101433056+ShangYuQiu@users.noreply.github.com>
2023-09-23 19:34:50 +0200

    Update Evaluador.java

--- src/main/java/logic/Evaluador.java
+++ src/main/java/logic/Evaluador.java
@@ -9,5 +9,21 @@ package logic;
  * @author shangyu
  */
 public class Evaluador {
+
+
+    private Mano _mano;
+    private ArrayList<String> draw;
     
+    public Evaluador(Mano mano){
+        _mano = mano;
+    }
+    
+    public void evaluar(){
+    
+        List<Carta> c = _mano.getCartas();
+        
+        
+        
+    
+    }
 }
