c856f0b162aa24c498555ec2f070984e7ee0b802
XinXiangLinZhou <100683297+XinXiangLinZhou@users.noreply.github.com>
2023-09-29 19:49:45 +0200

    Update Apartado2.java

--- src/main/java/control/commands/Apartado2.java
+++ src/main/java/control/commands/Apartado2.java
@@ -39,42 +39,18 @@ public class Apartado2 {
                     m.agregarCarta(c);
                 }
                 n=Integer.parseInt(carta[1]);
-                
-                //hay mas de 3 cartas comunes
-                if(n>3){
-                    formarCartas(n,carta[2],m);
-                }
-                else{
-                    //igual que apartado1
+               
                     for (int i = 0; i < carta[2].length(); i += 2) {
                     Carta c = new Carta(carta[0].substring(i, i + 1), carta[0].substring(i + 1, i + 2));
                     m.agregarCarta(c);
                     }
                 }
                 //salida=sol
-            }
+            
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
- private void formarCartas(int n,String carta, Mano m){
-                    for(int i=0;i<n;i+=2){
-                        Carta a = new Carta(carta.substring(i, i + 1), carta.substring(i + 1, i + 2));
-                        m.agregarCarta(a);
-                        for(int j=i+2;j<n*2-2;j+=2){
-                            Carta b = new Carta(carta.substring(j, j + 1), carta.substring(j + 1, j + 2));
-                            m.agregarCarta(b);
-                            for(int k=j+2;k<n*2;k+=2){
-                                 Carta c = new Carta(carta.substring(k, k + 1), carta.substring(k + 1, k + 2));
-                                  m.agregarCarta(c);
-                                //comparar mejor mano para obtener mejorCarta
-                                //sol=mejor carta
-                                //eliminar c
-                            }
-                            //eliminar b
-                        }
-                        //eliminar a
-                    }
-    }
+
 }
