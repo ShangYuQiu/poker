839c4a95383a09232da18e4d1ea99f3994cd5b5f
XinXiangLinZhou <100683297+XinXiangLinZhou@users.noreply.github.com>
2023-10-02 14:19:52 +0200

    Update Apartado2.java

--- src/main/java/control/commands/Apartado2.java
+++ src/main/java/control/commands/Apartado2.java
@@ -5,14 +5,15 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.BufferedWriter;
+import java.io.FileWriter;
+import logic.Evaluador;
 import objects.Mano;
 import objects.Carta;
-
+import java.io.IOException;
 public class Apartado2 {
 
-    private String entrada;
-    private Mano mano;
-    private String salida;
+    private final String entrada;
+    private final String salida;
 
     public Apartado2(String entra, String sal) {
         this.entrada = entra;
@@ -28,28 +29,51 @@ public class Apartado2 {
             InputStreamReader fr = new InputStreamReader(fis);
             BufferedReader in = new BufferedReader(fr);
 
-            String carta[];
+            String card;
             int n;
-            while ((carta = in.readLine().split(";")) != null) {
-
+            while ((card = in.readLine()) != null) {
+                String carta[]=card.split(";");
                 Mano m = new Mano();
+                //carta propia
                 for (int i = 0; i < carta[0].length(); i += 2) {
-                    // no se si esta bien
                     Carta c = new Carta(carta[0].substring(i, i + 1), carta[0].substring(i + 1, i + 2));
                     m.agregarCarta(c);
                 }
                 n=Integer.parseInt(carta[1]);
-               
-                    for (int i = 0; i < carta[2].length(); i += 2) {
-                    Carta c = new Carta(carta[0].substring(i, i + 1), carta[0].substring(i + 1, i + 2));
+                //carta comun
+               for (int i = 0; i < carta[2].length(); i += 2) {
+                    Carta c = new Carta(carta[2].substring(i, i + 1), carta[2].substring(i + 1, i + 2));
                     m.agregarCarta(c);
+                }
+                this.ev = new Evaluador();
+                ev.setMano(m);
+                ev.evaluar();
+                }
+                FileWriter f_salida = new FileWriter(salida, true);
+                try (//salida
+                        BufferedWriter writer = new BufferedWriter(f_salida)) {
+                        writer.append(carta[0]+";"+carta[1]+";"+carta[2]);
+                        writer.newLine();
+                        //mejor mano
+                        writer.append("- Best Hand: ");
+                        writer.append(m.getJugada().getDescripcion());
+                        writer.newLine();
+                        //si carta comun es 5 no hay draw
+                        if(n!=5){
+                            for (String s : m.getDraws()) {
+                                writer.append("- " + s);
+                                writer.newLine();
+                            }
+                        }
+                        writer.newLine();
+                        writer.close();
                     }
                 }
-                //salida=sol
+            in.close();
             
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.print(e);
         }
     }
 
