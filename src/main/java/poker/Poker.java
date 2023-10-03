14c32522c438bfd91b0beb8f8424049597cdd6e5
XinXiangLinZhou <100683297+XinXiangLinZhou@users.noreply.github.com>
2023-10-03 15:56:12 +0200

    Update Poker.java

--- src/main/java/poker/Poker.java
+++ src/main/java/poker/Poker.java
@@ -12,6 +12,7 @@ public class Poker {
             case "1" -> controller.run(1);
             case "2" -> controller.run(2);
             case "3" -> controller.run(3);
+            case "4" -> controller.run(4);
             default -> {
                 System.out.println("Primer argumento introducido debe ser entre 1 y 3.");
             }
